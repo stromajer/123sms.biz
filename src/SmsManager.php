9eeb53dfaa45a8f6d8db32c70fb0054713ee0869
stromajer <stromajer.michal@gmail.com>
2017-02-04 20:21:29 +0100

    added response codes meanings

--- src/SmsManager.php
+++ src/SmsManager.php
@@ -10,6 +10,24 @@
 
 class SmsManager
 {
+
+    //response codes meanings
+    const RESPONSE_SMS_ACCEPTED_AND_SHEDULED = 2,
+        RESPONSE_SMS_ACCEPTED = 1,
+        RESPONSE_SMS_ACCEPTED_LOW_CREDIT_WARN = 0,
+        RESPONSE_SMS_INVALID_CREDENTIALS = -1,
+        RESPONSE_SMS_INSUFFICIENT_CREDIT = -2,
+        RESPONSE_SMS_INACTIVE_NUMBER = -3,
+        RESPONSE_SMS_UNPAID_BILL_NUMBER_INACTIVE = -4,
+        RESPONSE_SMS_INVALID_NUMBER = -5,
+        RESPONSE_SMS_INVALID_SENDER = -6,
+        RESPONSE_SMS_INVALID_TEXT = -7,
+        RESPONSE_SMS_TOO_LONG_TEXT = -8,
+        RESPONSE_SMS_HLR_INVALID_NUMBER = -9,
+        RESPONSE_SMS_TIME_LIMIT_EXCEEDED = -10,
+        RESPONSE_SMS_UNKNOWN_ERROR = -11;
+
+
     /**
      * url for posting sms
      */
@@ -79,7 +97,7 @@ class SmsManager
     public function __construct()
     {
         if (!function_exists('curl_init')) {
-           throw new \Exception('php-curl extension missing');
+            throw new \Exception('php-curl extension missing');
         }
 
         $this->curl = curl_init();
@@ -99,7 +117,7 @@ class SmsManager
     public function addRecipient($number)
     {
         if (is_array($number)) {
-            $mergedNumbers = array_merge($this->recipients,$number);
+            $mergedNumbers = array_merge($this->recipients, $number);
             $this->recipients = array_unique($mergedNumbers);
 
             return $this;
@@ -135,7 +153,7 @@ class SmsManager
      */
     public function setCredentials($username, $password = null, $apiKey = null)
     {
-        if (!$password && ! $apiKey) {
+        if (!$password && !$apiKey) {
             throw new \Exception('Password or API key is mandatory');
         }
 
@@ -161,9 +179,9 @@ class SmsManager
     private function credentialsOk()
     {
         if ($this->username) {
-           if ($this->password || $this->apiKey ) {
-               return true;
-           }
+            if ($this->password || $this->apiKey) {
+                return true;
+            }
             throw new \Exception('Set password or API key.');
         }
         throw new \Exception('Set username.');
@@ -191,9 +209,9 @@ class SmsManager
      */
     private function senderOk()
     {
-         if ($this->getSender()) {
-             return true;
-         }
+        if ($this->getSender()) {
+            return true;
+        }
 
         throw new \Exception('No sender was set.');
     }
@@ -227,7 +245,7 @@ class SmsManager
         $this->curlConfig[CURLOPT_POSTFIELDS]['password'] = $this->getPassword() ? $this->getPassword() : $this->getApiKey();
         curl_setopt_array($this->curl, $this->curlConfig);
 
-        $xml =  simplexml_load_string(curl_exec($this->curl));
+        $xml = simplexml_load_string(curl_exec($this->curl));
 
         return $xml;
     }
@@ -237,7 +255,7 @@ class SmsManager
      *
      * @param string number
      * @return \SimpleXMLElement
-    */
+     */
     public function verifyNumber($number)
     {
         $this->credentialsOk();
@@ -248,7 +266,7 @@ class SmsManager
 
         curl_setopt_array($this->curl, $this->curlConfig);
 
-        $xml =  simplexml_load_string(curl_exec($this->curl));
+        $xml = simplexml_load_string(curl_exec($this->curl));
 
         return $xml;
     }
@@ -291,12 +309,12 @@ class SmsManager
      */
     public function setSender($sender)
     {
-        if (strlen($sender) <= self::MAX_SENDER_NAME_LENGTH ) {
+        if (strlen($sender) <= self::MAX_SENDER_NAME_LENGTH) {
             $this->sender = $sender;
             return $this;
         }
 
-        throw new \Exception('Sender name exceeds maximum length ( '.self::MAX_SENDER_NAME_LENGTH.' )');
+        throw new \Exception('Sender name exceeds maximum length ( ' . self::MAX_SENDER_NAME_LENGTH . ' )');
     }
 
     /**
@@ -358,5 +376,25 @@ class SmsManager
     public function getSender()
     {
         return $this->sender;
+    }
+
+    public function getResponseMeanings()
+    {
+        return [
+            self::RESPONSE_SMS_ACCEPTED_AND_SHEDULED,
+            self::RESPONSE_SMS_ACCEPTED,
+            self::RESPONSE_SMS_ACCEPTED_LOW_CREDIT_WARN,
+            self::RESPONSE_SMS_INVALID_CREDENTIALS,
+            self::RESPONSE_SMS_INSUFFICIENT_CREDIT,
+            self::RESPONSE_SMS_INACTIVE_NUMBER,
+            self::RESPONSE_SMS_UNPAID_BILL_NUMBER_INACTIVE,
+            self::RESPONSE_SMS_INVALID_NUMBER,
+            self::RESPONSE_SMS_INVALID_SENDER,
+            self::RESPONSE_SMS_INVALID_TEXT,
+            self::RESPONSE_SMS_TOO_LONG_TEXT,
+            self::RESPONSE_SMS_HLR_INVALID_NUMBER,
+            self::RESPONSE_SMS_TIME_LIMIT_EXCEEDED,
+            self::RESPONSE_SMS_UNKNOWN_ERROR,
+        ];
     }
 }
